d6abf806de078dc430f0bb37d827453379b82b18
nanangbsi <19221458@bsi.ac.idgit config --global user.email 19221458@bsi.ac.id>
2023-11-27 14:44:35 +0700

    first init github

---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Repost by © 2023 by <a href="https://www.instagram.com/itsmeaw22/" target="_blank">Fashion Frenzy</a></span>
+		</div>
+	</div>
+</footer>
